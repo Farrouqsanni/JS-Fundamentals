7d54e1311578ca246f3b785ac548f46a5ff92bbf
Farrouq Sanni <sannifarrouq@gmail.com>
2025-09-15 15:07:22 +0100

    Add task 2 - arguments

---
+++ 2-arguments.js
@@ -0,0 +1,20 @@
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log("0 argument");
+} else if (args.length === 1) {
+  console.log("1 argument");
+} else {
+  console.log(`${args.length} arguments`);
+}
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
+
+
